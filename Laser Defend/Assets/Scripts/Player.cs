e600ca659acfd6c3df37fa69e4b0c6097532b005
ash-gamedev <adrouill1997@gmail.com>
2022-02-06 16:25:38 -0500

    Add projectiles and shooters

--- Laser Defend/Assets/Scripts/Player.cs
+++ Laser Defend/Assets/Scripts/Player.cs
@@ -3,65 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
-{
-    // Serializable variables
-    [Header("Input")]
-    [SerializeField] private float moveSpeed = 0.3f;
-
-    [Header("Boundaries Padding")]
-    [SerializeField] private float paddingLeft;
-    [SerializeField] private float paddingRight;
-    [SerializeField] private float paddingTop;
-    [SerializeField] private float paddingBottom;
-
-    // private variables
-    private Vector2 rawInput;
-    private Vector2 minBounds;
-    private Vector2 maxBounds;
-
-    #region Start, Update
-    void Start()
+namespace Assets.Scripts {
+    public class Player : MonoBehaviour
     {
-        InitBounds();
+        // Serializable variables
+        [Header("Input")]
+        [SerializeField] private float moveSpeed = 0.3f;
+
+        [Header("Boundaries Padding")]
+        [SerializeField] private float paddingLeft;
+        [SerializeField] private float paddingRight;
+        [SerializeField] private float paddingTop;
+        [SerializeField] private float paddingBottom;
+
+        // private variables
+        Shooter shooter;
+        private Vector2 rawInput;
+        private Vector2 minBounds;
+        private Vector2 maxBounds;
+
+        #region Awake, Start, Update
+        void Awake()
+        {
+            shooter = GetComponent<Shooter>();
+        }
+
+        void Start()
+        {
+            InitBounds();
+        }
+
+        void Update()
+        {
+            Move();
+        }
+        #endregion
+
+        #region private functions
+        void InitBounds()
+        {
+            // Get the boundaries of the screen through the camera
+            Camera mainCamera = Camera.main;
+            minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+            maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
+            // Add padding so user can't go off screen
+            minBounds += new Vector2(paddingLeft, paddingBottom);
+            maxBounds -= new Vector2(paddingRight, paddingTop);
+        }
+
+        void OnMove(InputValue value)
+        {
+            rawInput = value.Get<Vector2>();
+        }
+
+        void Move()
+        {
+            // multiply input by move speed, and deltaTime (for framerate independent)
+            Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+            Vector2 newPos = new Vector2();
+
+            // use Clamp to restrict the movement depending on the boundaries
+            newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x, maxBounds.x);
+            newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y, maxBounds.y);
+
+            transform.position = newPos;
+        }
+
+        void OnFire(InputValue value)
+        {
+            if(shooter != null)
+            {
+                shooter.isFiring = value.isPressed;
+            }
+        }
+        #endregion
     }
-
-    void Update()
-    {
-        Move();
-    }
-    #endregion
-
-    #region private functions
-    void InitBounds()
-    {
-        // Get the boundaries of the screen through the camera
-        Camera mainCamera = Camera.main;
-        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
-        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
-
-        // Add padding so user can't go off screen
-        minBounds += new Vector2(paddingLeft, paddingBottom);
-        maxBounds -= new Vector2(paddingRight, paddingTop);
-    }
-
-    void OnMove(InputValue value)
-    {
-        rawInput = value.Get<Vector2>();
-    }
-
-    void Move()
-    {
-        // multiply input by move speed, and deltaTime (for framerate independent)
-        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
-        Vector2 newPos = new Vector2();
-
-        // use Clamp to restrict the movement depending on the boundaries
-        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x, maxBounds.x);
-        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y, maxBounds.y);
-
-        transform.position = newPos;
-    }
-
-    #endregion
 }
+
---
+++ Laser Defend/Assets/Scripts/Shooter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Shooter : MonoBehaviour
+    {
+        // Serializable fields
+        [Header("General")]
+        [SerializeField] GameObject projectilePrefab;
+        [SerializeField] float baseFiringRate = 0.5f;
+        [SerializeField] float projectileSpeed = 10f;
+        [SerializeField] float projectileLifetime = 5f;
+
+        [Header("AI")]
+        [SerializeField] bool useAI;
+        [SerializeField] float minimumFiringRate = 0.1f;
+        [SerializeField] float firingRateVariance = 0.2f;
+        
+        // public fields
+        [HideInInspector] public bool isFiring;
+
+        // private fields
+        private Coroutine firingCoroutine;
+
+        #region Start, Update
+        void Start()
+        {
+            if (useAI)
+                isFiring = true;
+        }
+
+        void Update()
+        {
+            Fire();
+        }
+        #endregion
+
+        #region private functions
+        /// <summary>
+        /// Begin firing projectiles
+        /// </summary>
+        void Fire()
+        {
+            if (isFiring && firingCoroutine == null)
+            {
+                firingCoroutine = StartCoroutine(FireContinuously());
+            }
+            else if (!isFiring && firingCoroutine != null)
+            {
+                //option 1 (hammer option): StopAllCoroutines();
+                //option 2: StopCoroutine()
+                StopCoroutine(firingCoroutine);
+                firingCoroutine = null;
+            }
+        }
+
+        /// <summary>
+        /// Couroutine to continuously fires projectiles
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator FireContinuously()
+        {
+            while (true)
+            {
+                GameObject instance = Instantiate(projectilePrefab,  // what object to instantiate
+                        transform.position, // where to spawn the object
+                        Quaternion.identity // need to specify rotation
+                        );
+
+                Rigidbody2D instanceRb = instance.GetComponent<Rigidbody2D>();
+                if (instanceRb != null)
+                    instanceRb.velocity = (useAI ? -transform.up : transform.up) * projectileSpeed;
+
+                Destroy(instance, projectileLifetime);
+
+                float timeTillNextProjectile;
+                if (!useAI) 
+                { 
+                    timeTillNextProjectile = baseFiringRate;
+                }
+                else
+                {
+                    float newFiringRate = UnityEngine.Random.Range( baseFiringRate - firingRateVariance, baseFiringRate + firingRateVariance );
+                    timeTillNextProjectile = Mathf.Clamp(newFiringRate, minimumFiringRate, float.MaxValue);
+                }
+
+                yield return new WaitForSeconds(timeTillNextProjectile);
+            }
+        }
+        #endregion
+    }
+}
